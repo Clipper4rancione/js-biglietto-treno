323acc4ae63478b57a204da7ab6a80c653169136
Clipper4rancione <meluziis.ciro99@gmail.com>
2022-10-11 18:13:00 +0200

    finished script

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ const userKilometers = parseInt(prompt('Quanti km vorresti percorrere?'));
 const userAge = parseInt(prompt('Quanti anni hai?'));
 const priceForKm = 0.21;
 
+let ticketPrice = (priceForKm * userKilometers);
+
 //Input dei km inseriti in index.html
 document.getElementById('km-count').innerHTML = ` 
     Il tuo percorso è lungo: ${userKilometers}km
@@ -15,12 +17,14 @@ document.getElementById('age').innerHTML = `
 ` ;
 
 //calcolo prezzo del biglietto
-let ticketPrice = (priceForKm * userKilometers);
+
 
 //calcolo prezzo del biglietto scontato per età
-if(userAge <= 17){
-    let ticketPrice = (priceForKm * userKilometers * 20 / 100)
-}else
+if(userAge < 18){
+     ticketPrice = (ticketPrice * 20 / 100)
+}else if(userAge > 65){
+     ticketPrice = (ticketPrice * 40 / 100)
+}else{};
 
 
 //output prezzo del biglietto finale
