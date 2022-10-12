26cf269fd6b2f4dbd870a5c77da155eab80ae057
Clipper4rancione <meluziis.ciro99@gmail.com>
2022-10-12 15:56:31 +0200

    [add] correction to the ticket logic

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
 // domande all'utente
 
-const userKilometers = parseInt(prompt('Quanti km vorresti percorrere?'));
-const userAge = parseInt(prompt('Quanti anni hai?'));
+const userKilometers = prompt('Quanti km vorresti percorrere?');
+const userAge = prompt('Quanti anni hai?');
 const priceForKm = 0.21;
 
-let ticketPrice = (priceForKm * userKilometers);
+const juniorDiscount = 0.2;
+const seniorDiscount = 0.4;
+
+let msg, ticketPrice, discountPrice
+
+ticketPrice = userKilometers * priceForKm;
+
+msg = `Il prezzo del biglietto è di € ${ticketPrice.toFixed(2)}.`
 
 //Input dei km inseriti in index.html
 document.getElementById('km-count').innerHTML = ` 
@@ -16,18 +23,20 @@ document.getElementById('age').innerHTML = `
     Anni: ${userAge}
 ` ;
 
-//calcolo prezzo del biglietto
-
-
 //calcolo prezzo del biglietto scontato per età
 if(userAge < 18){
-     ticketPrice = (ticketPrice * 20 / 100)
-}else if(userAge > 65){
-     ticketPrice = (ticketPrice * 40 / 100)
-}else{};
-
+     discountPrice = ticketPrice * (1 - juniorDiscount);
+     msg += `
+     <br>
+     Dato che sei minorenne hai diritto a uno sconto del ${juniorDiscount * 100}% quindi il totale del tuo biglietto è di € ${discountPrice.toFixed(2)}
+     `
+}else if(userAge >= 65){
+    discountPrice = ticketPrice * (1 - seniorDiscount);
+    msg += `
+    <br>
+    Dato che sei over 65 hai diritto a uno sconto del ${seniorDiscount * 100}% quindi il totale del tuo biglietto è di € ${discountPrice.toFixed(2)}
+    `
+};
 
 //output prezzo del biglietto finale
-document.getElementById('result').innerHTML = `
-    Il tuo biglietto costerà: ${ticketPrice}€
-`;
+document.getElementById('result').innerHTML = msg;
